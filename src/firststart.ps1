9878ceece087a791ccd381095bc875f5a4e4e9f1
Tino Hager <tino.hager@nager.at>
2021-01-24 22:37:29 +0100

    Update firststart.ps1

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -1,5 +1,7 @@
 Write-Host "hello"
 
 Add-Content $Env:Temp\autounattend.log "Github Powershell executed"
+Add-Content C:\Temp\autounattend1.log "Github Powershell executed"
+New-Item -Path "C:\" -Name "Github" -ItemType "directory"
 
 sleep 600
