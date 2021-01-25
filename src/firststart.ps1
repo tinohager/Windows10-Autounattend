b6f25bb1dcc97e3942c0ab5b92b7e099fc5021c9
Tino Hager <tino.hager@nager.at>
2021-01-25 11:59:53 +0100

    Update firststart.ps1

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -4,7 +4,7 @@ New-Item -Path "C:\" -Name "Temp" -ItemType "directory"
 New-Item -Path "C:\Temp" -Name "Unattend" -ItemType "directory"
 Add-Content "C:\Temp\Unattend\autounattend.log" "Github Powershell executed"
 
-$Installer7Zip = $env:TEMP + "C:\Temp\Unattend\7z1900-x64.msi"; 
-Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip; 
-msiexec /i $Installer7Zip /qb; 
-Remove-Item $Installer7Zip;
+$Installer7Zip = "C:\Temp\Unattend\7z1900-x64.msi"
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip
+msiexec /i $Installer7Zip /qb
+#Remove-Item $Installer7Zip
