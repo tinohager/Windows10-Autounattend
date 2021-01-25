6847dfdd86c798ca9ac938bd84783eac2f8ebae4
Tino Hager <tino.hager@nager.at>
2021-01-25 09:20:41 +0100

    Update firststart.ps1

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -4,4 +4,8 @@ Add-Content $Env:Temp\autounattend.log "Github Powershell executed"
 Add-Content C:\Temp\autounattend1.log "Github Powershell executed"
 New-Item -Path "C:\" -Name "Github" -ItemType "directory"
 
-sleep 600
+$Installer7Zip = $env:TEMP + "\7z1900-x64.msi"; 
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile 
+$Installer7Zip; 
+msiexec /i $Installer7Zip /qb; 
+Remove-Item $Installer7Zip;
