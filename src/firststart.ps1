194baa97849311f73baab1a4438e2ccf89f2a2be
Tino Hager <tino.hager@nager.at>
2021-01-25 11:41:04 +0100

    Update firststart.ps1

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -1,11 +1,10 @@
-Write-Host "hello"
+Write-Host "FirstLogonCommands Script"
 
-Add-Content $Env:Temp\autounattend.log "Github Powershell executed"
-Add-Content C:\Temp\autounattend1.log "Github Powershell executed"
-New-Item -Path "C:\" -Name "Github" -ItemType "directory"
+New-Item -Path "C:\" -Name "Temp" -ItemType "directory"
+New-Item -Path "C:\Temp" -Name "Unattend" -ItemType "directory"
+Add-Content "C:\Temp\Unattend\autounattend.log" "Github Powershell executed"
 
-$Installer7Zip = $env:TEMP + "\7z1900-x64.msi"; 
-Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile 
-$Installer7Zip; 
+$Installer7Zip = $env:TEMP + "C:\Temp\Unattend\7z1900-x64.msi"; 
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip; 
 msiexec /i $Installer7Zip /qb; 
 Remove-Item $Installer7Zip;
