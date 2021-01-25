cdbbcaec71f0725841a171918c43e981c99b16cd
Tino Hager <tino.hager@nager.at>
2021-01-25 21:28:11 +0100

    Optimize install script

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -1,10 +1,18 @@
 Write-Host "FirstLogonCommands Script"
 
-New-Item -Path "C:\" -Name "Temp" -ItemType "directory"
-New-Item -Path "C:\Temp" -Name "Unattend" -ItemType "directory"
-Add-Content "C:\Temp\Unattend\autounattend.log" "Github Powershell executed"
+$installPath = "C:\Temp\Unattend"
 
-$Installer7Zip = "C:\Temp\Unattend\7z1900-x64.msi"
-Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip
-msiexec /i $Installer7Zip /qb
-#Remove-Item $Installer7Zip
+New-Item $installPath -ItemType Directory
+
+# Install 7-Zip
+$installer7Zip = Join-Path -Path $installPath -ChildPath "7z1900-x64.msi"
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $installer7Zip
+Start-Process "msiexec.exe" -Wait -NoNewWindow -ArgumentList "/i ""$($installer7Zip)"" /qb"
+
+# Install Notepad++
+$installerNotepadPlusPlus = Join-Path -Path $installPath -ChildPath "npp.7.9.2.Installer.x64.exe"
+Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.2/npp.7.9.2.Installer.x64.exe" -OutFile $installerNotepadPlusPlus
+Start-Process $installerNotepadPlusPlus -Wait -NoNewWindow -ArgumentList "/S"
+
+$manufacturer = (Get-ComputerInfo | Select -expand CsManufacturer)
+Add-Content "C:\Temp\Unattend\autounattend.log" $manufacturer
