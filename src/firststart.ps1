047f60b26db78876fb32dc7673e58b0175eba413
Tino Hager <tino.hager@nager.at>
2021-01-27 14:27:34 +0100

    Update firststart.ps1

--- src/firststart.ps1
+++ src/firststart.ps1
@@ -5,11 +5,13 @@ $installPath = "C:\Temp\Unattend"
 New-Item $installPath -ItemType Directory
 
 # Install 7-Zip
+Write-Host "Install 7-Zip"
 $package7Zip = Join-Path -Path $installPath -ChildPath "7z1900-x64.msi"
 Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $package7Zip
 Start-Process "msiexec.exe" -Wait -NoNewWindow -ArgumentList "/i ""$($package7Zip)"" /qb"
 
 # Install Notepad++
+Write-Host "Install Notepad++"
 $packageNotepadPlusPlus = Join-Path -Path $installPath -ChildPath "npp.7.9.2.Installer.x64.exe"
 Invoke-WebRequest "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.2/npp.7.9.2.Installer.x64.exe" -OutFile $packageNotepadPlusPlus
 Start-Process $packageNotepadPlusPlus -Wait -NoNewWindow -ArgumentList "/S"
@@ -17,6 +19,11 @@ Start-Process $packageNotepadPlusPlus -Wait -NoNewWindow -ArgumentList "/S"
 $manufacturer = (Get-ComputerInfo | Select -expand CsManufacturer)
 Add-Content "C:\Temp\Unattend\autounattend.log" $manufacturer
 if ($manufacturer -eq "Lenovo") {
+  # Install Vantage
   $packageLenovoCommercialVantage = Join-Path -Path $installPath -ChildPath "LenovoCommercialVantage.zip"
   Invoke-WebRequest "https://download.lenovo.com/pccbbs/thinkvantage_en/metroapps/Vantage/LenovoCommercialVantage_10.2011.14.0_v1.zip" -OutFile $packageLenovoCommercialVantage
+  $lenovoCommercialVantage = Join-Path -Path $installPath -ChildPath "LenovoCommercialVantage"
+  Expand-Archive $packageLenovoCommercialVantage -DestinationPath $lenovoCommercialVantage
+  $installScriptLenovoCommercialVantage = Join-Path -Path $lenovoCommercialVantage -ChildPath "setup-commercial-vantage.bat"
+  Start-Process $installScriptLenovoCommercialVantage -Wait -NoNewWindow
 }
